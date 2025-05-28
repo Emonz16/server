c4a0a8fef37c2711270ed1496df0bd2b3ee03900
Emon <jakariachowdhury2427@gmail.com>
2025-05-28 22:07:06 +0600

    change

--- index.js
+++ index.js
@@ -9,9 +9,7 @@ const port = process.env.PORT || 3000;
 app.use(cors());
 app.use(express.json());
 
-
-
-
+// Connection URI
 
 const uri = `mongodb+srv://${process.env.DB_NAME}:${process.env.DB_KEYS}@cluster0.ppuciiw.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0`;
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -31,18 +29,28 @@ async function run() {
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    //await client.close();
   }
 }
 run().catch(console.dir);
 
-
+const db = client.db('myDatabase').collection('myCollection'); // Replace 'myCollection' with your collection name
 
 
 app.get('/', (req, res) => {
   res.send('Welcome to the API!');
 }
 );
+
+app.post('/add', async (req, res) => { 
+  const newData = req.body;
+  console.log(newData);
+  const result = await db.insertOne(newData);
+  res.send(`Data added successfully with ID: ${result}`);
+
+})
+
+
 
 app.listen(port, () => {
      console.log (`server is running on port  ${port}`);
